f62c7b6d0767178f236d990b14575cd714525b99
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-08-15 21:51:13 +0200

    Add gallery and catCard to components

---
+++ src/components/gallery/catCard.tsx
@@ -0,0 +1,3 @@
+export default function CatCard() {
+  return <img src="https://cdn2.thecatapi.com/images/80v.gif" alt="Random Cat" />;
+}
---
+++ src/components/gallery/gallery.tsx
@@ -0,0 +1,15 @@
+import CatCard from './catCard';
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing cats</h1>
+      <CatCard />
+      <CatCard />
+      <CatCard />
+      <CatCard />
+      <CatCard />
+      <CatCard />
+    </section>
+  );
+}
