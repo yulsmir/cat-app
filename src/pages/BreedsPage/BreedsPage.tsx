9dea4734dd81aed2c953ae8f46cf716ca3ce23aa
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-09-10 22:18:35 +0200

    Add temp colors for navigation

--- src/pages/BreedsPage/BreedsPage.tsx
+++ src/pages/BreedsPage/BreedsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Headline from '../../components/Headline/Headline';
 
 function BreedsPage() {
