854ca72541cc4df8021dd6096b66d5c736b1558f
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-09-11 13:27:02 +0200

    Fix footer

--- src/pages/VotePage/VotePage.tsx
+++ src/pages/VotePage/VotePage.tsx
@@ -4,7 +4,7 @@ import RandomCatImage from '../../components/RandomCatImage/RandomCatImage';
 function VotePage() {
   return (
     <section>
-      <Headline element="h5" className="welcome-text">
+      <Headline element="h4" className="welcome-text">
         Vote for a cat
       </Headline>
       <RandomCatImage />
