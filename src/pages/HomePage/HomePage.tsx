279821dbe393f1acfe8ff530b7a121b1978bd81b
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-09-11 18:30:39 +0200

    Add structure to a random image at home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,24 @@
 import Headline from '../../components/Headline/Headline';
 import RandomCatImage from '../../components/RandomCatImage/RandomCatImage';
+import Button from '../../components/Button/Button';
 
 function HomePage() {
   return (
     <section>
-      <Headline element="h2" className="welcome-text">
-        Welcome to my CatApp.
-      </Headline>
-      <Headline element="h3" className="welcome-text">
-        Everyday is a Caturday.
-      </Headline>
-      <RandomCatImage />
+      <div>
+        <Headline element="h2" className="welcome-text">
+          Welcome to my CatApp.
+        </Headline>
+        <Headline element="h3" className="welcome-text">
+          Everyday is a Caturday.
+        </Headline>
+      </div>
+      <div>
+        <Button>Random cat</Button>
+      </div>
+      <div>
+        <RandomCatImage />
+      </div>
     </section>
   );
 }
