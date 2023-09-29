32b48903162b22ca038df1aefb27e2262ea22954
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-09-29 15:14:44 +0200

    Fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,9 @@
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
-import CatWalk from '../CatWalk/CatWalk';
 
 function Header() {
   return (
     <header>
-      {/* <CatWalk /> */}
       <Navigation />
     </header>
   );
