5ba4e3087b7226835605bbc06d1b54ea3d98c8df
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-10-02 01:07:26 +0200

    Fix typo in files names

--- src/pages/VotePage/VotePage.tsx
+++ src/pages/VotePage/VotePage.tsx
@@ -1,7 +1,7 @@
 import Headline from '../../components/Headline/Headline';
 import RandomCatImage from '../../components/RandomCatImage/RandomCatImage';
 import Counter from '../../components/Counter/Counter';
-import './VotePaage.css';
+import './VotePage.css';
 
 function VotePage() {
   return (
