18fe91559c03ecdee845a8065dabc0bd0ea87701
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-10-02 01:05:15 +0200

    Fix style of hamburger menu

--- src/pages/ImagesSearchPage/ImagesSearchPage.tsx
+++ src/pages/ImagesSearchPage/ImagesSearchPage.tsx
@@ -1,13 +1,38 @@
-import Headline from '../../components/Headline/Headline';
+import { useEffect, useState } from 'react';
+import './ImageSearchPage.css';
 
-function ImagesSearchPage() {
+const ImageGrid = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const url = `https://api.thecatapi.com/v1/images/search?limit=9`;
+    const api_key = 'DEMO_API_KEY';
+
+    fetch(url, {
+      headers: {
+        'x-api-key': api_key,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setImages(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <section>
-      <Headline element="h3" className="welcome-text">
-        Images Search Page
-      </Headline>
-    </section>
+    <div className="image-grid">
+      {images.map((imageData, index) => (
+        <div key={index} className="col col-lg">
+          <img src={imageData.url} alt={`Cat ${index}`} />
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
-export default ImagesSearchPage;
+export default ImageGrid;
