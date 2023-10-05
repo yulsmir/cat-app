accea561a715c95c91414f3128da1e75b126282a
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-10-05 22:38:46 +0200

    Add useMemo to a breedList

--- src/components/BreedsList/BreedsList.tsx
+++ src/components/BreedsList/BreedsList.tsx
@@ -1,16 +1,22 @@
 import { BreedsListProps } from './BreedsList.types';
+import { useMemo } from 'react';
 
 import './BreedsList.css';
 
 function BreedsList({ data, ...rest }: BreedsListProps) {
+  // Use useMemo to memoize the mapped array of breeds
+  const mappedData = useMemo(() => {
+    return data?.map((item, index) => (
+      <li key={index} className="list-item">
+        {item}
+        {/* console.log(item); */}
+      </li>
+    ));
+  }, [data]); // Re-run when data changes
+
   return (
     <ul {...rest} className="list">
-      {data?.map((data, index) => (
-        <li key={index} className="list-item">
-          {data}
-          console.log(data);
-        </li>
-      ))}
+      {mappedData}
     </ul>
   );
 }
